685f129748a08a39208a84f59eae63c0661f4f04
Chalana001 <ckalhara7277@gmail.com>
2025-12-23 01:10:49 +0530

    initial commit

---
+++ src/main/java/controller/LogInFormController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LogInFormController {
+
+    @FXML
+    private Button btnLogIn;
+
+    @FXML
+    private Label lblErrorMsg;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void LogInButtonOnAction(ActionEvent event) {
+
+    }
+
+}
